504550d0e223a4ca549ec277ff2cc8bc27536954
Vil Rodriguez <vilmango@hotmail.com>
2019-12-16 16:56:54 -0500

    actualiza css

--- src/views/register-view.js
+++ src/views/register-view.js
@@ -28,7 +28,7 @@ export default () => {
             <input id="button-register" class="submit btn" type="submit" class="button-login" value="Registrar"></input>
           </div>
           <div class="links">
-            <p class="singup-text">¿Ya tienes una cuenta?&nbsp;<a class="link register" href="#/"><span id="" class="register-href">Iniciar Sesión</span></a></p>
+            <p class="singup-text">¿Ya tienes una cuenta?&nbsp;<a class="link register" href="#/"><span id="" class="link register-href">Iniciar Sesión</span></a></p>
           </div>
         </form>
       </div>
