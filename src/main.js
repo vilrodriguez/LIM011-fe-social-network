7f5e16627e9a4c9c31089f862d75af86034ad7a2
Vil Rodriguez <vilmango@hotmail.com>
2019-12-16 23:23:08 -0500

    actualiza main

--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
 import { changeView } from './controllers/route.js';
- import { firebaseConfig } from './firebase-controller/firebaseConfig.js';
-
+import { firebaseConfig } from './firebase-controller/firebaseConfig.js';
 
 const initRouter = () => {
   firebase.initializeApp(firebaseConfig);
   changeView(window.location.hash);
   window.addEventListener('hashchange', () => changeView(window.location.hash));
 };
-
-
 window.addEventListener('load', initRouter);
