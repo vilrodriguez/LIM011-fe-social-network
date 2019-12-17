79d5e142e1b1b950c59a5742906c5360b0b7da0d
MARYRIVERO <marilyndlvalle892286@gmail.com>
2019-12-17 16:13:44 -0500

    Agrega nueva funcion

--- src/firebase-controller/register-users.js
+++ src/firebase-controller/register-users.js
@@ -1,10 +1,14 @@
 export const createUser = (email, password) => {
+  // eslint-disable-next-line no-console
   console.log('gdgdhd', email, password);
   firebase.auth().createUserWithEmailAndPassword(email, password).catch((error) => {
     const errorCode = error.code;
     const errorMessage = error.message;
+    // eslint-disable-next-line no-console
     console.log(error.message);
+    // eslint-disable-next-line no-alert
     alert(errorCode);
+    // eslint-disable-next-line no-alert
     alert(errorMessage);
   });
 };
