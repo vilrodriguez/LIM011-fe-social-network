9133c698a1acfc887347dc1312a52ce99a623b5c
MARYRIVERO <marilyndlvalle892286@gmail.com>
2019-12-19 18:31:57 -0500

    cambia carpeta

--- src/firebase-controller/userAutentication.js
+++ src/firebase-controller/userAutentication.js
@@ -28,18 +28,18 @@ export const signInWithGoogle = () => {
   const provider = new firebase.auth.GoogleAuthProvider();
   firebase.auth().signInWithPopup(provider).then((result) => {
     // This gives you a Google Access Token. You can use it to access the Google API.
-    let token = result.credential.accessToken;
+    const token = result.credential.accessToken;
     // The signed-in user info.
-    let user = result.user;
+    const user = result.user;
     // ...
-}).catch((error) => {
+  }).catch((error) => {
     // Handle Errors here.
-    let errorCode = error.code;
-    let errorMessage = error.message;
+    const errorCode = error.code;
+    const errorMessage = error.message;
     // The email of the user's account used.
-    let email = error.email;
+    const email = error.email;
     // The firebase.auth.AuthCredential type that was used.
-    let credential = error.credential;
+    const credential = error.credential;
     // ...
-    });
+  });
 };
