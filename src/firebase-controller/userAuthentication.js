c8481b7630adfe454287064006e588c596470b07
Vil Rodriguez <vilmango@hotmail.com>
2019-12-19 18:51:29 -0500

    actualiza nombre

--- src/firebase-controller/userAuthentication.js
+++ src/firebase-controller/userAuthentication.js
@@ -23,25 +23,31 @@ export const signInUser = (email, password) => {
     alert(errorMessage);
   });
 };
-
 export const signInWithGoogle = () => {
   const provider = new firebase.auth.GoogleAuthProvider();
   firebase.auth().signInWithPopup(provider).then((result) => {
     // This gives you a Google Access Token. You can use it to access the Google API.
     const token = result.credential.accessToken;
+    console.log(token);
+
     // The signed-in user info.
     const user = result.user;
+    console.log(user);
     // ...
-}).catch((error) => {
+  }).catch((error) => {
     // Handle Errors here.
     const errorCode = error.code;
+    console.log(errorCode);
     const errorMessage = error.message;
+    console.log(errorMessage);
     // The email of the user's account used.
     const email = error.email;
+    console.log(email);
     // The firebase.auth.AuthCredential type that was used.
     const credential = error.credential;
+    console.log(credential);
     // ...
-    });
+  });
 };
 export const signInWithFacebook = () =>{
   const provider = new firebase.auth.FacebookAuthProvider();
--- src/views/login-view.js
+++ src/views/login-view.js
@@ -1,4 +1,4 @@
-import { signInUser, signInWithGoogle, signInWithFacebook } from '../firebase-controller/userAutentication.js';
+import { signInUser, signInWithGoogle, signInWithFacebook } from '../firebase-controller/userAuthentication.js';
 
 export default () => {
   const divElement = document.createElement('div');
