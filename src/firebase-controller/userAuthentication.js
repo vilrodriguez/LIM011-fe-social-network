ce8dcadb25f77db1e3493ffffa5b861bdec2d0c5
MARYRIVERO <marilyndlvalle892286@gmail.com>
2019-12-19 19:19:53 -0500

    cambios

--- src/firebase-controller/userAuthentication.js
+++ src/firebase-controller/userAuthentication.js
@@ -55,20 +55,20 @@ export const signInWithFacebook = () =>{
   const provider = new firebase.auth.FacebookAuthProvider();
   console.log(provider);
   firebase.auth().useDeviceLanguage();
-  firebase.auth().signInWithPopup(provider).then(function(result) {
+  firebase.auth().signInWithPopup(provider).then((result) => {
     // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-    var token = result.credential.accessToken;
+    const token = result.credential.accessToken;
     // The signed-in user info.
-    var user = result.user;
+    const user = result.user;
     // ...
-  }).catch(function(error) {
+  }).catch((error) => {
     // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
+    const errorCode = error.code;
+    const errorMessage = error.message;
     // The email of the user's account used.
-    var email = error.email;
+    const email = error.email;
     // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
+    const credential = error.credential;
     // ...
   });
-}
+};
