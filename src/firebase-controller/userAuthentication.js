48e3340b005fccbfb7bdf7dc2c2ea05557866ec0
Vil Rodriguez <vilmango@hotmail.com>
2020-01-02 14:57:33 -0500

    Actualiza userAuthentication, css y homeview

--- src/firebase-controller/userAuthentication.js
+++ src/firebase-controller/userAuthentication.js
@@ -30,23 +30,6 @@ export const signInUser = (email, password) => {
   });
 };
 export const signInWithGoogle = () => {
-<<<<<<< HEAD
-console.log('Para logearse con Google');
-
-};
-// export const signInWithFacebook = () => {
-//   const provider = new firebase.auth.FacebookAuthProvider();
-//   return firebase.auth().signInWithPopup(provider);
-// };
-
-export const signInWithFacebook = () => {
-  var provider = new firebase.auth.FacebookAuthProvider();
-  firebase.auth().useDeviceLanguage();
-
-  console.log('Para logearse con Facebook');
-  console.log(provider);
-  firebase.auth().signInWithPopup(provider).then(()=> {
-=======
   const provider = new firebase.auth.GoogleAuthProvider();
   firebase.auth().signInWithPopup(provider).then((result) => {
     // This gives you a Google Access Token. You can use it to access the Google API.
@@ -76,92 +59,21 @@ export const signInWithFacebook = () => {
   const provider = new firebase.auth.FacebookAuthProvider();
   firebase.auth().useDeviceLanguage();
   firebase.auth().signInWithPopup(provider).then((result) => {
->>>>>>> eac7d9db194ab8d78064b25a1a46d9f73588991c
     // This gives you a Facebook Access Token. You can use it to access the Facebook API.
     var token = result.credential.accessToken;
     // The signed-in user info.
-<<<<<<< HEAD
-    var user = result.user;
-    console.log(user);
-    console.log(token);
-    console.log('Te has logeado con FB');
-  }).catch((error)=> {
-=======
     const user = result.user;
     console.log("te has logeado con facebook");
     // ...
   }).catch((error) => {
->>>>>>> eac7d9db194ab8d78064b25a1a46d9f73588991c
     // Handle Errors here.
     var errorCode = error.code;
     var errorMessage = error.message;
     // The email of the user's account used.
     var email = error.email;
     // The firebase.auth.AuthCredential type that was used.
-<<<<<<< HEAD
-    var credential = error.credential;
-    console.log('Algo ha pasado :(');
-    console.log(errorCode);
-    console.log(errorMessage);
-    
-  });
-};
-
-
-
-
-// export const signInWithGoogle = () => {
-//   const provider = new firebase.auth.GoogleAuthProvider();
-//   firebase.auth().signInWithPopup(provider).then((result) => {
-//     // This gives you a Google Access Token. You can use it to access the Google API.
-//     const token = result.credential.accessToken;
-//     // The signed-in user info.
-//     const user = result.user;
-//     console.log('Te has logeado con Google');
-//     console.log(user);
-//     // ...
-//   }).catch((error) => {
-//     // Handle Errors here.
-//     const errorCode = error.code;
-//     const errorMessage = error.message;
-//     // The email of the user's account used.
-//     const email = error.email;
-//     // The firebase.auth.AuthCredential type that was used.
-//     const credential = error.credential;
-
-//     console.log(errorCode);
-//     console.log(errorMessage);
-//     console.log(email);
-//     console.log(credential);
-//   });
-// };
-// export const signInWithFacebook = () => {
-//   const provider = new firebase.auth.FacebookAuthProvider();
-//   /* eslint no-console: "error" */
-
-//   // custom console
-//   console.log(provider);
-//   firebase.auth().useDeviceLanguage();
-//   firebase.auth().signInWithPopup(provider).then((result) => {
-//     // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-//     const token = result.credential.accessToken;
-//     // The signed-in user info.
-//     const user = result.user;
-//     console.log('Te has logeado con Facebook');
-//   }).catch((error) => {
-//     // Handle Errors here.
-//     const errorCode = error.code;
-//     const errorMessage = error.message;
-//     // The email of the user's account used.
-//     const email = error.email;
-//     // The firebase.auth.AuthCredential type that was used.
-//     const credential = error.credential;
-//   });
-// };
-=======
     const credential = error.credential;
     console.log("no has podido ingresar con facebook");
     // ...
   });
 };
->>>>>>> eac7d9db194ab8d78064b25a1a46d9f73588991c
