9972832c06ff8a4ee594fe921a1425765e15eaba
Vil Rodriguez <vilmango@hotmail.com>
2020-01-02 15:44:34 -0500

    Actualiza css, main y nuevo profil-view

--- src/views/main-view.js
+++
@@ -1,5 +0,0 @@
-export default () => {
-  const viewMain = ` sasasaas
-    fdgdfgd
-    fghfghfg`;
-};
---
+++ src/views/profile-view.js
@@ -0,0 +1,7 @@
+export default () => {
+  const viewProfile = ` <h3>Edita la información en tu cuenta</h3>
+  <form></form>
+  `;
+  const divElement = document.createElement('section');
+  divElement.className ='container-profile';
+};
