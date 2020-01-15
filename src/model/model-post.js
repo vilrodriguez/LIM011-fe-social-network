7698925073433ad4448dd964fea41243ced6208c
Vil Rodriguez <vilmango@hotmail.com>
2020-01-14 22:23:32 -0500

    actualiza user observer

--- src/model/model-post.js
+++ src/model/model-post.js
@@ -14,7 +14,7 @@ export const userObserver = (userInformation) => {
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
       userInformation(user.uid);
-      console.log('usuario logueado', user);
+      // console.log('usuario logueado', user);
     } else {
       console.log('Ha cerrado sesión');
     }
