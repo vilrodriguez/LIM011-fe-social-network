67c1e77d76753b9606196a9e68ded12a8858adc0
Vil Rodriguez <vilmango@hotmail.com>
2020-01-15 01:34:13 -0500

    actualiza funcion de obtener post de database y render en html

--- src/model/model-post.js
+++ src/model/model-post.js
@@ -21,20 +21,53 @@ export const userObserver = (userInformation) => {
     }
   });
 };
-// get post content
-
-export const setupGuides = (data) =>{
+// render post content in html
+/*
+export const setupGuides = (data, box) =>{
+  let postBox = box;
     let template = '';
     data.forEach(doc =>{
-        const guide = doc.data();
-        console.log(guide);
-    })
-}
-export const getTextPost = () => {
+        const guides = doc.data();
+        // console.log(guides);
+        const div = `
+        <div class="title"> ${guides.title}</div>
+        <div class="title"> ${guides.content}</div>
+        `;
+        template += div;
+    });
+    postBox.innerHTML = template;
+    console.log(postBox);
+};
+// get post content from database
+export const getTextPost = (box) => {
   firebase.firestore().collection('guides').get().then(((snapshot) => {
-    setupGuides(snapshot.docs);
+    setupGuides(snapshot.docs, box);
+  }));
+};*/
+
+// changing var names into our actual names
+ export const setupPost = (data, postContent) =>{
+  let postTemplate = postContent;
+    let template = '';
+    data.forEach(doc =>{
+        const info = doc.data();
+        // console.log(guides);
+        const div = `
+        <div class="title"> ${info.postText}</div>
+        <div class="title"> ${info.private}</div>
+        `;
+        template += div;
+    });
+    postTemplate.innerHTML = template;
+    console.log(postTemplate);
+};
+export const getTextPost = (content) => {
+  firebase.firestore().collection('post').get().then(((snapshot) => {
+    setupPost(snapshot.docs, content);
   }));
 };
+
+
 
 // export const getPost = (callback) => firebase.firestore().collection('post')
 //   .onSnapshot((snapshot) => {
