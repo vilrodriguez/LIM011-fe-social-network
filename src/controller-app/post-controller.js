255b416d89bd6065ee69b2c34aaef313267ec41e
Vil Rodriguez <vilmango@hotmail.com>
2020-01-17 15:36:27 -0500

    actualiza plantilla de post

--- src/controller-app/post-controller.js
+++ src/controller-app/post-controller.js
@@ -7,12 +7,14 @@ export const setupPost = (data, postContent, user) => {
     const div = `
           <div id = "${doc.id}" class="box-publication-feed">
             <div class="box-publication-feed-header">
-              <span id ="poster-name">${user.Name} dice:</span>
+              <span id ="poster-name">${info.name} dice:</span>
             </div>
             <div class="box-publication-feed-text">${info.postText} </div>
             <div class="box-likes">
               <div class="text-likes">
                 <img class="heart-likes" src="./img/lover.svg" alt="Likes heart picture"/>
+                <button id="editar">Editar</button>
+                <button id="eliminar">Eliminar</button>
               </div>
             </div>
           </div>`;
